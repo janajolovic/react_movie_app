dc17dda46a796ff7b338176660a3994988fbb635
Jana Jolovic <janajolovic04@gmail.com>
2022-08-06 18:35:29 +0200

    changes in css

--- src/MovieContext.js
+++ src/MovieContext.js
@@ -1,19 +1,23 @@
 import { createContext, useState } from "react";
+import axios from "axios";
 
 const MovieContext = createContext();
 
 export function MovieProvider({ children }) {
   const [movies, setMovies] = useState([]);
 
+
   const fetchPopular = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=b454aa11fb4b5fc5b515d2e80a898a1c&language=en-US&page=1"
-    );
-    const movies = await data.json();
-    setMovies(movies.results);
-    console.log(movies.results)
-    console.log(movies)
-  }
+    try {
+      let response = await axios
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=b454aa11fb4b5fc5b515d2e80a898a1c&language=en-US&page=1`)
+        setMovies(response.data.results);
+        console.log(response.data.results)
+        console.log(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
 
   return (
     <MovieContext.Provider
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,17 +1,26 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Movie = ({movie}) => {
   return (
-    <div className="movie">
-      <Link to={`/movie/${movie.id}`} key={movie.id} />
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      layout
+      className="movie"
+    >
+      <Link to={`/movie/${movie.id}`} key={movie.id} >
+      <div className="shadow"></div>
+        </Link>
         {movie.poster_path !== null ? (
         <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} />
       ) : (
         <img src="" />
       )}
       <h2>{movie.title}</h2>
-    </div>
+    </motion.div>
   )
 }
 
