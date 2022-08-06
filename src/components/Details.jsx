0d1777688dc8dd496218d0d249ab6662a555c469
Jana Jolovic <janajolovic04@gmail.com>
2022-08-06 20:25:48 +0200

    movie details page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -25,20 +25,22 @@ const Details = () => {
   return (
     <div className="details_container">
         <img src={"https://image.tmdb.org/t/p/w500" + movie?.poster_path} />
-        <h1>{movie?.title}</h1>
-        <p><span>Overview: </span>{movie?.overview}</p> 
-        <span>Genres: </span>{movie?.genres.map(genre => {
-            return <p className="movie_genre" key={genre.id}>{genre.name}</p>
-        })}    
-        <span>Production companies: </span>{movie?.production_companies.map(comp => {
-            return <p key={comp.id}>{comp.name}</p>
-        })}    
-        <p><span>Status: </span>{movie?.status}</p>
-        <p><span>Release date: </span>{movie?.release_date}</p>
-        <p><span>Runtime: </span>{movie?.runtime}</p>
-        <p><span>Popularity: </span>{movie?.popularity}</p>
-        <p><span>Rating: </span>{movie?.vote_average}</p>  
-        <a href={movie?.homepage}>Film page</a>  
+        <div className='details'>
+            <h1>{movie?.title}</h1>
+            <p><span>Overview: </span>{movie?.overview}</p> 
+            <span>Genres: </span>{movie?.genres.map(genre => {
+                return <p className="movie_genre" key={genre.id}>{genre.name}</p>
+            })}    
+            <span>Production companies: </span>{movie?.production_companies.map(comp => {
+                return <p key={comp.id}>{comp.name}</p>
+            })}    
+            <p><span>Status: </span>{movie?.status}</p>
+            <p><span>Release date: </span>{movie?.release_date}</p>
+            <p><span>Runtime: </span>{movie?.runtime}</p>
+            <p><span>Popularity: </span>{movie?.popularity}</p>
+            <p><span>Rating: </span>{movie?.vote_average}</p>  
+            <a href={movie?.homepage}>Film page</a>  
+        </div>
     </div>
   )
 }
