87a2f72611c55ef89e8e230a028015126a200d7c
Jana Jolovic <janajolovic04@gmail.com>
2022-08-15 17:12:35 +0200

    search

---
+++ src/components/Search.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useContext } from "react";
+import MovieContext from "../MovieContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Search = () => {
+  const [value, setValue] = useState("");
+  const { fetchPopular, fetchSearch } = useContext(MovieContext);
+
+  const onKeyUp = (event) => {
+    
+  };
+
+  return (
+    <div className="search-movies">
+      <label htmlFor="search">
+        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" onClick={() => navigate("/")}/>
+      </label>
+      <input
+        type="text"
+        id="search"
+        placeholder="Search for movies"
+        onKeyDown={(e) => onKeyUp(e)}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default Search;
