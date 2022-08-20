dd5e3cd80b658d39a579671bb99c415fd1295351
Jana Jolovic <janajolovic04@gmail.com>
2022-08-20 19:59:56 +0200

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (event) => {
+    event.preventDefault();
+    setInputs({
+      ...inputs,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const signInHandler = (event) => {
+    event.preventDefault();
+    setInputs({
+      email: "",
+      password: "",
+    });
+
+    navigate("/");
+  };
+
   return (
-    <div className='login_container'>
-        <form>
-            <input 
-                placeholder='Email'
-                type="text"
-                name="password"
-            />
-            <input 
-                placeholder='Password'
-                type="password"
-                name='password'
-            />
-            <button 
-                type='submit'>
-                Login
-            </button>
-        </form>
-
+    <div className="login_container">
+      <div className="login_form">
+        <div className="login_left">
+          <h1>Login</h1>
+          <form onSubmit={signInHandler}>
+            <input
+              onChange={changeHandler}
+              value={inputs.email}
+              type="email"
+              placeholder="Email"
+              name="email"
+              id="email"
+            ></input>
+            <input
+              onChange={changeHandler}
+              value={inputs.password}
+              type="password"
+              placeholder="Password"
+              name="password"
+              id="password"
+            ></input>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        <div className="login_right">
+          <img src="https://ak15suthar.github.io/Portfolio/img/work.png"></img>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
