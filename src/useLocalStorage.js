7e4bb442c85cf1114ed674da6ab2e635f54964dc
Jana Jolovic <janajolovic04@gmail.com>
2022-08-24 18:12:33 +0200

    local storage

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -6,3 +6,16 @@ function getStorageValue(key, defaultValue) {
   const initial = JSON.parse(saved);
   return initial || defaultValue;
 }
+
+export const useLocalStorage = (key, defaultValue) => {
+    const [value, setValue] = useState(() => {
+      return getStorageValue(key, defaultValue);
+    });
+  
+    useEffect(() => {
+      // storing input name
+      localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+  
+    return [value, setValue];
+  };
